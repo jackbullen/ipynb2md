4eb8f6b18527c6462e2ffaa921bbebc8af8edc59
Jack <jackbullen@icloud.com>
2023-09-29 19:13:43 -0700

    base working

---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposableMd = vscode.commands.registerCommand('extension.convertNotebook', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const filePath = editor.document.uri.fsPath;
+            if (filePath.endsWith('.ipynb')) {
+                convertToMarkdown(filePath);
+            } else {
+                vscode.window.showErrorMessage('The current file is not a Jupyter Notebook');
+            }
+        } else {
+            vscode.window.showErrorMessage('No active editor found');
+        }
+    });
+
+    let disposableDocx = vscode.commands.registerCommand('extension.convertNotebookDocx', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const filePath = editor.document.uri.fsPath;
+            if (filePath.endsWith('.ipynb')) {
+                convertToDocx(filePath);
+            } else {
+                vscode.window.showErrorMessage('The current file is not a Jupyter Notebook');
+            }
+        } else {
+            vscode.window.showErrorMessage('No active editor found');
+        }
+    });
+
+    context.subscriptions.push(disposableMd);
+
+    context.subscriptions.push(disposableDocx);
+}
+
+function convertToMarkdown(filePath: string) {
+    const command = `jupyter nbconvert --to markdown ${filePath}`;
+    cp.exec(command, (error, stdout, stderr) => {
+        if (error) {
+            vscode.window.showErrorMessage('Failed to convert notebook to Markdown: ' + stderr);
+        } else {
+            vscode.window.showInformationMessage('Successfully converted notebook to Markdown');
+        }
+    });
+}
+
+function convertToDocx(filePath: string) {
+    const command = `pandoc ${filePath.replace('.ipynb', '.md')} -o ${filePath.replace('.ipynb', '.docx')}`;
+    cp.exec(command, (error, stdout, stderr) => {
+        if (error) {
+            vscode.window.showErrorMessage('Failed to convert notebook to Docx: ' + stderr);
+        } else {
+            vscode.window.showInformationMessage('Successfully converted notebook to Docx');
+        }
+    });
+}
+
+export function deactivate() {}
