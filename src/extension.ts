64ca25453b18f9fc8b63ff2d971de60cca9b4535
Jack <jackbullen@icloud.com>
2023-10-10 13:27:34 -0700

    Convert markdown to PDF with pandoc and xelatex

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import * as cp from 'child_process';
+import {exec} from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposableMd = vscode.commands.registerCommand('extension.convertNotebook', () => {
@@ -30,14 +30,42 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    let disposablePdf = vscode.commands.registerCommand('extension.convertMarkdownToPdf', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const filePath = editor.document.uri.fsPath;
+            if (filePath.endsWith('.md')) {
+                convertMarkdownToPdf(filePath);
+            } else {
+                vscode.window.showErrorMessage('The current file is not a Markdown file');
+            }
+        } else {
+            vscode.window.showErrorMessage('No active editor found');
+        }
+    });
+
+    context.subscriptions.push(disposablePdf);
+
     context.subscriptions.push(disposableMd);
 
     context.subscriptions.push(disposableDocx);
 }
 
+function convertMarkdownToPdf(filePath: string) {
+    const pdfPath = filePath.replace('.md', '.pdf');
+    const command = `pandoc "${filePath}" --pdf-engine=xelatex -o "${pdfPath}"`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            vscode.window.showErrorMessage('Failed to convert notebook to Markdown: ' + stderr);
+        } else {
+            vscode.window.showInformationMessage('Successfully converted Markdown to PDF');
+        }
+    });
+}
+
 function convertToMarkdown(filePath: string) {
     const command = `jupyter nbconvert --to markdown ${filePath}`;
-    cp.exec(command, (error, stdout, stderr) => {
+    exec(command, (error, stdout, stderr) => {
         if (error) {
             vscode.window.showErrorMessage('Failed to convert notebook to Markdown: ' + stderr);
         } else {
@@ -48,7 +76,7 @@ function convertToMarkdown(filePath: string) {
 
 function convertToDocx(filePath: string) {
     const command = `pandoc ${filePath.replace('.ipynb', '.md')} -o ${filePath.replace('.ipynb', '.docx')}`;
-    cp.exec(command, (error, stdout, stderr) => {
+    exec(command, (error, stdout, stderr) => {
         if (error) {
             vscode.window.showErrorMessage('Failed to convert notebook to Docx: ' + stderr);
         } else {
